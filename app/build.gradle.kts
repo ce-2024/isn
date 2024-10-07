9acc20101023a5dcab267e9d11ecc47bb9f33f3c
issamux <issamux@gmail.com>
2024-10-07 15:13:34 +0100

    [Android] App skeleton

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.instantsystem.android.newsapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.instantsystem.android.newsapp"
+        minSdk = 28
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    lint {
+        abortOnError = true
+        ignoreWarnings = false
+        checkDependencies = true
+        ignoreTestSources = false
+        checkAllWarnings = true
+        warningsAsErrors = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    // compose
+    implementation(platform(libs.androidx.compose.bom))
+}
