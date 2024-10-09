9fe047e6d0737e7b43aff48ce2d98fe7778df178
issamux <issamux@gmail.com>
2024-10-09 05:03:35 +0100

    [Android] Fix instrumentation ui test
    TODO: check why the two tests fail when execute simultaneously, but they succeed when started one by one.

--- feature/news/src/androidTest/kotlin/com/instantsystem/android/feature/news/test/ui/NewsArticleUiScreenTest.kt
+++ feature/news/src/androidTest/kotlin/com/instantsystem/android/feature/news/test/ui/NewsArticleUiScreenTest.kt
@@ -2,67 +2,80 @@ package com.instantsystem.android.feature.news.test.ui
 
 import android.content.Context
 import android.content.res.Configuration
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.platform.app.InstrumentationRegistry
+import com.instantsystem.android.feature.news.di.NEWS_HTTP_CLIENT
 import com.instantsystem.android.feature.news.ui.NewsHomeScreen
 import com.instantsystem.android.feature.news.ui.tag.NewsHomeScreenTestTags
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.Json
 import org.junit.Rule
+import org.koin.core.context.loadKoinModules
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import org.koin.test.KoinTest
 import java.util.Locale
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class NewsArticleUiScreenTest : KoinTest {
+
+    private val context by lazy {
+        InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    private val newsHttpClientWithNoApi = module {
+        factory(named(NEWS_HTTP_CLIENT)) {
+            HttpClient(CIO) {
+                install(ContentNegotiation) {
+                    json(Json {
+                        ignoreUnknownKeys = true
+                    })
+                }
+            }
+        }
+    }
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @BeforeTest
-    fun setUp() {
-//        val dispatcher = UnconfinedTestDispatcher()
-//        Dispatchers.setMain(dispatcher)
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun when_news_app_launched_and_locale_is_fr_assert_empty_screen_displayed() = runTest {
+        setLocale(context, Locale.FRANCE)
+        assertEquals(Locale.getDefault(), Locale.FRANCE)
+        with(composeTestRule) {
+            setContent {
+                NewsHomeScreen()
+            }
+            waitUntilExactlyOneExists(hasText("No articles for country : ${Locale.FRANCE.country}"))
+            onNodeWithTag(
+                NewsHomeScreenTestTags.NEWS_ARTICLE_EMPTY_SCREEN
+            ).assertIsDisplayed()
+        }
     }
 
+    @OptIn(ExperimentalTestApi::class)
     @Test
-    fun test_news_app_given_country_us_assert_empty_list_articles() = runTest {
-        composeTestRule.setContent {
-            NewsHomeScreen()
+    fun when_news_app_launched_and_api_key_missing_assert_error_screen_displayed() = runTest {
+        loadKoinModules(newsHttpClientWithNoApi)
+        with(composeTestRule) {
+            setContent {
+                NewsHomeScreen()
+            }
+            waitUntilExactlyOneExists(hasText("Some error happened!!"))
+            // Check if the error screen is displayed
+            onNodeWithTag(NewsHomeScreenTestTags.NEWS_ARTICLE_ERROR_SCREEN)
+                .assertIsDisplayed()
         }
-        composeTestRule.onNodeWithTag(
-            NewsHomeScreenTestTags.NEWS_ARTICLE_ITEM_SCREEN
-        ).assertIsDisplayed()
-    }
-
-    @Test
-    fun test_news_app_given_country_fr_assert_empty_list_articles() = runTest {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        setLocale(context, Locale("zyw"))
-        composeTestRule.setContent {
-            NewsHomeScreen()
-        }
-
-        composeTestRule.onNodeWithTag(
-            NewsHomeScreenTestTags.NEWS_ARTICLE_EMPTY_SCREEN
-        ).assertIsDisplayed()
-    }
-
-    @Test
-    fun test_news_app_given_max_request_response_assert_news_error_displayed() = runTest {
-        composeTestRule.setContent {
-            NewsHomeScreen()
-        }
-
-        Thread.sleep(2000)
-        // Check if the error screen is displayed
-        composeTestRule.onNodeWithTag(NewsHomeScreenTestTags.NEWS_ARTICLE_ERROR_SCREEN)
-            .assertIsDisplayed()
-
     }
 
     private fun setLocale(context: Context, locale: Locale) {
@@ -71,12 +84,6 @@ class NewsArticleUiScreenTest : KoinTest {
         val config = Configuration(resources.configuration)
         config.setLocale(locale)
         context.createConfigurationContext(config)
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @AfterTest
-    fun tearDown() {
-        //Dispatchers.resetMain()
     }
 
 
